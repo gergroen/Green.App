1550d180cbf2d354b7ae62109decccfa7cab81ad
gergroen <gergroen@gmail.com>
2013-06-12 20:51:12 +0200

    Added WebApiService

--- src/Green.App.ServiceWebApi/Program.cs
+++ src/Green.App.ServiceWebApi/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Topshelf;
+using log4net.Config;
 
 namespace Green.App.ServiceWebApi
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            XmlConfigurator.Configure();
+            HostFactory.Run(x =>
+                {
+                    x.Service<WebApiService>(s =>
+                        {
+                            s.ConstructUsing(name => new WebApiService());
+                            s.WhenStarted(tc => tc.Start());
+                            s.WhenStopped(tc => tc.Stop());
+                        });
+                    x.RunAsLocalSystem();
+                    x.SetDescription("GreenAppWebApiService");
+                    x.SetDisplayName("GreenAppWebApiService");
+                    x.SetServiceName("GreenAppWebApiService");
+                    x.UseLog4Net();
+                });
         }
     }
 }
---
+++ src/Green.App.ServiceWebApi/WebApiService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+using log4net;
+
+namespace Green.App.ServiceWebApi
+{
+    public class WebApiService
+    {
+        private readonly ILog _logger = LogManager.GetLogger(typeof(WebApiService));
+
+        public string Url { get; set; }
+        public int MaxReceivedMessageSize { get; set; }
+
+        private HttpSelfHostServer _server;
+
+        public WebApiService()
+        {
+            Url = "http://localhost/Default";
+            MaxReceivedMessageSize = int.MaxValue;
+        }
+
+        public void Start()
+        {
+            _logger.Info("Starting service");
+
+            var address = new Uri(Url);
+            var config = new HttpSelfHostConfiguration(address);
+
+            config.MaxReceivedMessageSize = MaxReceivedMessageSize;
+            config.Routes.MapHttpRoute("ActionApi", "{controller}/{action}");
+
+            _server = new HttpSelfHostServer(config);
+
+            try
+            {
+                _server.OpenAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex);
+                throw;
+            }
+
+            _logger.Info("Service started");
+            _logger.InfoFormat("Listening on {0}", Url);
+        }
+
+        public void Stop()
+        {
+            _logger.Info("Stopping service");
+
+            _server.CloseAsync().Wait();
+            _server.Dispose();
+
+            _logger.Info("Service stopped");
+        }
+    }
+}
