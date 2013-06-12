ab2f84b9b48fa29c9c572e440eb7d628dc14938a
gergroen <gergroen@gmail.com>
2013-06-12 23:05:11 +0200

    Added Green.App.Test

---
+++ src/Green.App.Test/ServiceWebApi/TestWebApiService.cs
@@ -0,0 +1,31 @@
+﻿using Green.App.ServiceWebApi;
+using Green.App.Test.Helpers;
+using NUnit.Framework;
+
+namespace Green.App.Test.ServiceWebApi
+{
+    [TestFixture]
+    public class TestWebApiService
+    {
+        private WebApiService _webApiService;
+
+        [SetUp]
+        public void StartWebApiService()
+        {
+            _webApiService = new WebApiService("http://localhost/testapi");
+            _webApiService.Start();
+        }
+
+        [Test]
+        public void IsOnlineTest()
+        {
+            Assert.AreEqual("true", HttpWebRequestHelper.CreateHttpWebRequest("http://localhost/testapi/isonline", "GET"));
+        }
+
+        [TearDown]
+        public void StopWebApiService()
+        {
+            _webApiService.Stop();
+        }
+    }
+}
