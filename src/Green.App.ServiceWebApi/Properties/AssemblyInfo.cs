4837a320f50ba7208cdd7d78488cee357a8d72ae
gergroen <gergroen@gmail.com>
2013-06-29 00:17:20 +0200

    Renamed project to Green.App.Service

--- src/Green.App.ServiceWebApi/Properties/AssemblyInfo.cs
+++ src/Green.App.ServiceWebApi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Green.App.ServiceWebApi")]
+[assembly: AssemblyTitle("Green.App.Service")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Green.App.ServiceWebApi")]
+[assembly: AssemblyProduct("Green.App.Service")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
